4837521f1aa742b6d936f4cbb2369b5271c4858e
robby <robby@Robby-pc>
2012-10-05 17:42:59 +0300

    Update

--- js/some.js
+++
@@ -1,9 +0,0 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-document.write('<scr'+'ipt type="text/javascript" src="../../js/jquery-1.7.2.js" ></scr'+'ipt>');
-document.write('<scr'+'ipt type="text/javascript" src="../../js/jquery.dataTables.min.js" ></scr'+'ipt>');
-document.write('<scr'+'ipt type="text/javascript" src="../../js/header_footer_remover.js" ></scr'+'ipt>');
-
