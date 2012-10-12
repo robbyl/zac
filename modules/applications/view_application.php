bc1a5d455f21663eb200e3f3d7739050f9c2b9c1
josephmarcel <josephmarcel@joseph>
2012-10-12 12:48:52 +0300

    change password

---
+++ modules/applications/view_application.php
@@ -0,0 +1,223 @@
+<?php require '../../includes/session_validator.php'; ?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="icon" href="../../favicon.ico" type="image/x-icon" />
+        <title>SOFTBILL | VIEW APPLICATION</title>
+        <link href="../../css/layout.css" rel="stylesheet" type="text/css">
+        <link href="../../css/tooltip.css" rel="stylesheet" type="text/css">
+
+        <style>
+
+            #fieldset tr:nth-child(even){
+                background: #f2f2f2;
+            }
+
+            #fieldset tr:nth-child(odd){
+                background: #fff;
+            }
+
+        </style>
+
+        <script src="../../js/jquery-1.7.2.js" type="text/javascript"></script>
+        <script src="../../js/softbill-core.js" type="text/javascript"></script>
+        <script src="../../js/tooltip.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.message, .error').hide().slideDown('normal').click(function(){
+                    $(this).slideUp('normal');
+                });
+                
+                $('#view-application').click(function(){
+                    var id = $('#appln_id').val();
+                    var printWin = window.open('print_application.php?id='+id,'','left=0,top=0,width=1000,height=600,toolbar=no,scrollbars=no,status=no');
+                    printWin.focus();    
+                });
+                
+                $('#pdf').click(function(){
+                    savePDF('save-pdf', '../../css/sheet.css');
+                });
+                
+                $('.tooltip').tipTip({
+                    delay: "300"
+                });
+            });
+        </script>
+
+    </head>
+
+    <body>
+        <div class="container">
+            <?php require '../../includes/header.php'; ?>
+            <div class="sidebar">
+                <ul class="nav">
+                    <li><a href="../../home.php" class="home">Home</a></li>
+                    <li> <a href="../users/users.php" class="users">Manage Users</a></li>
+                    <li> <a href="../settings/settings.php" class="settings">Settings</a> </li>
+                    <li> <a href="../applications/applications.php" class="applications">Applications</a>
+                        <ul>
+                            <li><a href="../applications/add_new_appln.php">Add application</a></li>
+                        </ul>
+                    </li>
+                    <li> <a href="../customers/customers.php" class="customers">Customers</a></li>
+                    <li> <a href="../meters/meters.php" class="meters">Water Meters</a></li>
+                    <li> <a href="../invoice/invoices.php" class="invoices">Invoice</a></li>
+                    <li> <a href="../paypoint/paypoint.php" class="financial">Pay Point</a></li>
+                    <li> <a href="../report/reports.php" class="reports">Reports</a></li>
+                </ul>
+                <!-- end .sidebar --></div>
+            <div class="content">
+                <?php
+                // Displaying message and errors
+                include '../../includes/info.php';
+                require '../../functions/general_functions.php';
+
+                if (isset($_GET['id']) && !empty($_GET['id'])) {
+
+                    // Getting applicantion data form the database
+                    require '../../config/config.php';
+
+
+                    $id = clean($_GET['id']);
+
+                    $query_appln = "SELECT appln_id, appln_type, appln_date, engeneer_appr,
+                                           appnt_fullname, appnt_types, billing_areas,
+                                           surveyed_date, approved_date, inspected_by,
+                                           premise_nature, service, occupants, appnt_tel, appnt_post_addr,
+                                           appnt_phy_addr, block_no, plot_no,living_area, living_town
+                                      FROM application appln
+                                 LEFt JOIN applicant appnt
+                                        ON appln.appnt_id = appnt.appnt_id
+                                 LEFT JOIN appnt_type apnty
+                                        ON appnt.appnt_type_id = apnty.appnt_type_id
+                                 LEFT JOIN service_nature sev
+                                        ON appln.service_nature_id = sev.service_nature_id
+                                 LEFT JOIN billing_area ba
+                                        ON appnt.ba_id = ba.ba_id
+                                     WHERE appln_id = '$id'";
+
+                    $result_appln = mysql_query($query_appln) or die(mysql_error());
+                    $row = mysql_fetch_array($result_appln);
+                    $num_row = mysql_num_rows($result_appln);
+                    if ($num_row > 0) {
+                        ?>
+
+
+                        <form action="../../includes/pdf.php" method="post" id="html-form">
+                            <input type="hidden" name="html" id="html">
+                        </form>
+                        <h1><?php echo $row['appnt_fullname'] ?> Application Details</h1>
+                        <div class="actions" style="top: 100px; width: auto; right: 0; margin: 0 15px 0 0">
+                            <button class="print tooltip" accesskey="P" title="Print [Alt+Shift+P]" id="view-application">Print</button>
+                            <button class="pdf tooltip" accesskey="D" title="Save as PDF [Alt+Shift+D]" id="pdf">PDF</button>
+                        </div>
+                        <div class="hr-line"></div>
+                        <div id="save-pdf">
+                            <div id="fieldset">
+                                <fieldset>
+                                    <legend>Applicant Details</legend>
+                                    <input type="hidden" value="<?php echo $id; ?>" id="appln_id">
+                                    <table width="" border="0" cellpadding="5" cellspacing="0">
+                                        <tr>
+                                            <td width="300">Applicant Type:</td>
+                                            <td width="500"><strong><?php echo $row['appnt_types'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170" style="vertical-align: top">Full Name:</td>
+                                            <td width="300"><strong><?php echo $row['appnt_fullname'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Occupants/No of people:</td>
+                                            <td><strong><?php echo $row['occupants'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Phone Number:</td>
+                                            <td><strong><?php echo $row['appnt_tel'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Postal Address:</td>
+                                            <td><strong><?php echo $row['appnt_post_addr'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Physical Address:</td>
+                                            <td><strong><?php echo $row['appnt_phy_addr'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Block Number:</td>
+                                            <td><strong><?php echo $row['block_no'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Plot Number:</td>
+                                            <td><strong><?php echo $row['plot_no'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Living Area:</td>
+                                            <td><strong><?php echo $row['living_area'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Living Town:</td>
+                                            <td><strong><?php echo $row['living_town'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Billing Area/Zone:</td>
+                                            <td><strong><?php echo $row['billing_areas'] ?></strong></td>
+                                        </tr>
+                                    </table>
+                                </fieldset>
+                                <fieldset>
+                                    <legend>Application Details</legend>
+                                    <table width="" border="0" cellpadding="5" cellspacing="0">
+                                        <tr>
+                                            <td width="300">Application Date:</td>
+                                            <td width="500"><strong><?php echo date('Y-m-d'); ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Application Type:</td>
+                                            <td><strong><?php echo $row['appln_type'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Inspected By:</td>
+                                            <td><strong><?php echo $row['inspected_by'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Surveyed Date:</td>
+                                            <td><strong><?php echo $row['surveyed_date'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Engineer Approval:</td>
+                                            <td><strong><?php echo $row['engeneer_appr']; ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Approved Date:</td>
+                                            <td><strong><?php echo $row['approved_date'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Premises Nature:</td>
+                                            <td><strong><?php echo $row['premise_nature'] ?></strong></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="170">Service Nature:</td>
+                                            <td><strong><?php echo $row['service'] ?></strong></td>
+                                        </tr>
+                                    </table>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <?php
+                    } else {
+                        info('error', 'No records for this appln');
+                        header('Location: applications.php');
+                    }
+                } else {
+                    info('error', 'Application id not provided. Please try again');
+                    header('Location: applications.php');
+                }
+                ?>
+                <!-- end .content --></div>
+            <?php include '../../includes/footer.php'; ?>
+            <!-- end .container -->
+        </div>
+    </body>
+</html>
---
+++ modules/customers/process_add_customer.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * 2012 Flight
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is protected by copyright
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file.
+ *
+ *  @author Robert Londo <robbyl@ovi.com>
+ *  @copyright  2012 Flight
+ *  @version  Release: 1.0.0
+ */
+
+require '../../config/config.php';
+require '../../functions/general_functions.php';
+
+session_start();
+$user_id = $_SESSION['user_id'];
+session_commit();
+
+$appln_id = clean($_POST['appln_id']);
+$appnt_id = clean($_POST['appnt_id']);
+$ba_id = clean($_POST['ba_id']);
+
+$meter_number = clean($_POST['meter_number']);
+$premise_status = clean($_POST['premise_status']);
+$pay_center = clean($_POST['pay_center']);
+
+$query_customer = "INSERT INTO customer
+                               (pay_center, ba_id, premise_status,
+                                added_date, appln_id, appnt_id, added_by,
+                                cust_status)
+                        VALUES ('$pay_center', '$ba_id', '$premise_status',
+                                CURRENT_TIMESTAMP(), '$appln_id', '$appnt_id', '$user_id',
+                                'Connected')";
+
+$result_customer = mysql_query($query_customer) or die(mysql_error());
+
+$cust_id = mysql_insert_id();
+$acc_no = strtoupper(substr(uniqid(), 3));
+
+$query_acc = "INSERT INTO account
+                          (acc_no, cust_id)
+                   VALUES ('$acc_no', '$cust_id')";
+$result_acc = mysql_query($query_acc) or die(mysql_error());
+
+if (!empty($meter_number)) {
+
+    $met_met_cust = "INSERT INTO meter_customer
+                             (met_id, cust_id)
+                      VALUES ('$meter_number', '$cust_id')";
+    $result_met_cust = mysql_query($met_met_cust) or die(mysql_error());
+
+    $update_meter = "UPDATE meter
+                        SET availability = 'ISSUED'
+                      WHERE met_id = '$meter_number'";
+    $result_meter = mysql_query($update_meter) or die(mysql_error());
+}
+
+$query_appln = "UPDATE application
+                   SET status = 'Processed'
+                 WHERE appln_id = '$appln_id'";
+
+$result_appln = mysql_query($query_appln) or die(mysql_error());
+
+if ($result_meter || $result_customer && $result_acc && $result_appln || $result_met_cust) {
+
+    info('message', 'Customer seccessfully added!');
+    header('Location: ../applications/applications.php');
+} else {
+
+    info('error', 'Cannot add customer. Please try again');
+    header('Location: ../applications/applications.php');
+}
+?>
