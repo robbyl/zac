8ad7ba160ffb2d9f2e2433ebd6fd73ad96b222cd
robby <robby@Robby-pc>
2012-10-18 00:17:54 +0300

    added

---
+++ modules/data/form1.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+        <title>SOFTBILL</title>
+        <link rel="stylesheet" type="text/css" href="css/forms.css" />
+        <script src="js/jquery-1.7.2.js" type="text/javascript"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.message-outer, .error-outer').hide().slideDown('normal');
+            });
+        </script>
+    </head>
+
+    <body>
+        
+        <div class="data-form-wapper">
+            <div class="section">
+                
+            <!-- end .section  --></div>
+      <!-- end .data-form-wrapper  -->  </div>
+        
+    </body>
+</html>
