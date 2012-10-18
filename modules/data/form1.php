9ccada7bb53bf36de093e45825d60b94d9403394
robby <robby@Robby-pc>
2012-10-18 09:34:53 +0300

    update

--- modules/data/form1.php
+++ modules/data/form1.php
@@ -14,7 +14,7 @@
         <script src="js/jquery-1.7.2.js" type="text/javascript"></script>
 
         <script type="text/javascript">
-            $(document).ready(function(){
+            $(document).ready(function() {
                 $('.message-outer, .error-outer').hide().slideDown('normal');
             });
         </script>
@@ -29,83 +29,247 @@
                 <div class="section">
                     <h3>A. FORM HEAD</h3>
                     <table width="100%" border="1" cellspacing="0">
-  <tr>
-    <td colspan="2">&nbsp;</td>
-    <td colspan="3">&nbsp;</td>
-    <td>CD1</td>
-  </tr>
-  <tr>
-    <td colspan="2">&nbsp;</td>
-    <td colspan="3">&nbsp;</td>
-    <td>CD2</td>
-  </tr>
-  <tr>
-    <td colspan="2">&nbsp;</td>
-    <td colspan="3">&nbsp;</td>
-    <td>CD3</td>
-  </tr>
-  <tr>
-    <td colspan="2">&nbsp;</td>
-    <td colspan="3">&nbsp;</td>
-    <td>CD4</td>
-  </tr>
-  <tr>
-    <td colspan="2" rowspan="2">&nbsp;</td>
-    <td colspan="2">&nbsp;</td>
-    <td>&nbsp;</td>
-    <td rowspan="2">CD5</td>
-  </tr>
-  <tr>
-    
-    <td colspan="3">&nbsp;</td>
-  </tr>
-  <tr>
-    <td colspan="2">&nbsp;</td>
-    <td colspan="3">&nbsp;</td>
-    <td>CD6</td>
-  </tr>
-  <tr>
-    <td colspan="2">&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>CD7</td>
-  </tr>
-  <tr>
-    <td colspan="2">&nbsp;</td>
-    <td colspan="3">&nbsp;</td>
-    <td>CD8</td>
-  </tr>
-  <tr>
-    <td rowspan="3">&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>males</td>
-    <td>females</td>
-    <td>total</td>
-    <td rowspan="3">CD9</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td colspan="2">&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>CD10</td>
-  </tr>
-</table>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td colspan="3">&nbsp;</td>
+                            <td>CD1</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td colspan="3">&nbsp;</td>
+                            <td>CD2</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td colspan="3">&nbsp;</td>
+                            <td>CD3</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td colspan="3">&nbsp;</td>
+                            <td>CD4</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" rowspan="2">&nbsp;</td>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td rowspan="2">CD5</td>
+                        </tr>
+                        <tr>
 
+                            <td colspan="3">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td colspan="3">&nbsp;</td>
+                            <td>CD6</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>CD7</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td colspan="3">&nbsp;</td>
+                            <td>CD8</td>
+                        </tr>
+                        <tr>
+                            <td rowspan="3">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>males</td>
+                            <td>females</td>
+                            <td>total</td>
+                            <td rowspan="3">CD9</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>CD10</td>
+                        </tr>
+                    </table>
                     <!-- end .section  --></div>
+                <div class="section">
+                    <h3>B. HIV PREVENTION SERVICES</h3>
+                    <table width="100%" border="1" cellspacing="0">
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td colspan="5">Number of persons reached</td>
+                            <td rowspan="9">HP1</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td colspan="2">&nbsp;</td>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>Male</td>
+                            <td>Female</td>
+                            <td>Male</td>
+                            <td>Female</td>
+                            <td>Total</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </table>
+
+                    <h5>HIV PREVENTION AMONGST GENERAL POPULATION</h5>
+                    <table width="100%" border="1" cellspacing="0">
+                        <tr>
+                            <td colspan="2" rowspan="3">Type of intervation</td>
+                            <td colspan="5">Number of persons reached</td>
+                            <td rowspan="9">HP2</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Male</td>
+                            <td>Female</td>
+                            <td>Male</td>
+                            <td>Female</td>
+                            <td>Total</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </table>
+                    <table width="100%" border="1" cellspacing="0">
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </table>
+
+                </div>
                 <!-- end .data-form-wrapper  -->  </div>
         </form>
 
