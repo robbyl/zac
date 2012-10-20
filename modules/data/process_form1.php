8a95f2476cf263c86067b88f0d391fa2cd4075d8
robby <robby@Robby-pc>
2012-10-20 17:13:26 +0300

    update

--- modules/data/process_form1.php
+++ modules/data/process_form1.php
@@ -3,8 +3,12 @@
 require '../../config/config.php';
 require '../../functions/general_functions.php';
 
+// Geting form-data for data-section A data-set CD
 $some = clean($_POST['some']);
 
+// Geting form-data for data-section B data-set HP
+$some = clean($_POST['some']);
 
-
+// Geting form-data for data-section C data-set M
+$some = clean($_POST['some']);
 ?>
