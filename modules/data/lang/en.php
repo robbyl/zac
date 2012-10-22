0d8241bd8222bb140e095fd408b5872015618736
robby <robby@Robby-pc>
2012-10-22 12:23:54 +0300

    added

---
+++ modules/data/lang/en.php
@@ -0,0 +1,20 @@
+<?php
+
+$text[SECT_HEAD_A] = "INFORMATION ABOUT YOUR ORGANISATION";
+$text[SECT_HEAD_B] = "HIV PREVENTION SERVICES";
+$text[SECT_HEAD_C] = "HIV IMPACT MITIGATION SERVICES";
+$text[SECT_HEAD_D] = "HIV and AIDS CARE AND SUPPORT SERVICES";
+$text[SECT_HEAD_E] = "TRAINING AND CAPACITY BUILDING FOR HIV";
+$text[SECT_HEAD_F] = "MANAGEMENT AND COORDINATION OF HIV INTERVENTIONS";
+
+$text[SECT_SUB_HEAD_P1] = "HIV Prevention amongst vulnerable and high risk groups";
+$text[SECT_SUB_HEAD_P2] = "HIV Prevention amongst general population";
+$text[SECT_SUB_HEAD_P3] = "HIV Radio and TV";
+$text[SECT_SUB_HEAD_P4] = "Educators-trained and registered";
+$text[SECT_SUB_HEAD_P4] = "Educators-active";
+$text[SECT_SUB_HEAD_P6] = "IEC materials";
+$text[SECT_SUB_HEAD_CS1] = "Home based care volunteers";
+$text[SECT_SUB_HEAD_TC1] = "Training of staff and volunteers";
+$text[SECT_SUB_HEAD_TC2] = "Training of community level organisations";
+$text[SECT_SUB_HEAD_MC6] = "Areas covered in work plan";
+?>
