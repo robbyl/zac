1f9dc3d23d77bb592249a594a9e39574e21756e2
robby <robby@Robby-pc>
2012-10-28 13:34:20 +0300

    update

--- modules/printouts/print_organisations.php
+++ modules/printouts/print_organisations.php
@@ -241,7 +241,10 @@ require '../../includes/session_validator.php';
 
                     if ($details === "all") {
 
-                        $query_people = "SELECT `OrganisationCode`, `FullName`, `Designation`
+                        $person = array();
+
+                        $query_people = "SELECT `OrganisationCode`, `FullName`, `Designation`,
+                                                `Phone`, `Email`
                                            FROM tblgenorganisationpeople";
                         $result_people = mysql_query($query_people) or die(mysql_error());
 
@@ -250,11 +253,16 @@ require '../../includes/session_validator.php';
                         }
                     }
 
-                    $query_org = "SELECT `OrganisationName`, `OrganisationCode`, `PostalAddress`, `Phone`, `Fax`, `Email`,
-                                          DATE(`StartedOperating`) AS StartDAte, `OrganisationGroup`, `OrganisationCategoryDescription`
+                    $query_org = "SELECT `OrganisationName`, org.`OrganisationCode`, `PostalAddress`, org.`Phone`, org.`Fax`, org.`Email`, `PhysicalAddress`,
+                                          DATE(`StartedOperating`) AS StartDAte, `OrganisationGroup`, `OrganisationCategoryDescription`,
+                                          foc.`FullName` AS ZHAPMosFocal, hiv.`FullName` AS HIVPerson
                                      FROM tblgenorganisations org
                                INNER JOIN tblgensetuporganisationcategories cat
                                        ON org.`OrganisationCategoryID` = cat.`OrganisationCategoryID`
+                                LEFT JOIN tblgenorganisationpeople foc
+                                       ON org.`ZHAPMoSFocalPersonID` = foc.`OrganisationPersonID`
+                                LEFT JOIN tblgenorganisationpeople hiv
+                                       ON org.`HIVFocalPersonID` = hiv.`OrganisationPersonID`
                                           {$filter}";
 
                     $result_org = mysql_query($query_org) or die(mysql_error());
@@ -274,10 +282,10 @@ require '../../includes/session_validator.php';
                                 <div class="sheet-wraper">
                                     <div class="sheet-header">
                                         <div class="header-title">
-                                            <p style="font-weight: bold"><?php // echo $row_authority['aut_name']      ?></p>
+                                            <p style="font-weight: bold"><?php // echo $row_authority['aut_name']             ?></p>
                                             <p style="font-size: 18px; font-weight: bold">CASH DEPOSIT SLIP</p>
                                             <div class="page-logo">
-        <!--                                                <img src="../settings/logo/<?php // echo $row_authority['logo']      ?>" height="80">-->
+        <!--                                                <img src="../settings/logo/<?php // echo $row_authority['logo']             ?>" height="80">-->
                                             </div>
                                         </div>
                                         <!-- end .sheet-header --></div>
@@ -285,48 +293,66 @@ require '../../includes/session_validator.php';
                                         <p><strong>Print Date: </strong><span style="font-weight: normal;"><?php echo date('d M, Y') ?></span></p>
                                     </div>
                                     <div class="black-separator"></div>
-                                    <div class="sheet-table">
-                                        <table cellpadding="3" cellspacing="0" border="1" width="100%">
+                                    <div>
+                                        <table cellpadding="3" cellspacing="0" border="1" width="100%" class="two-groups">
                                             <?php
                                             $total = 0;
 
                                             foreach ($groups as $OrganisationCategoryDescription => $OrganisationCode) {
 
-                                                echo '<tr><th colspan="6">' . $OrganisationCategoryDescription . '</th></tr>';
-                                                echo '<tr>';
+                                                echo '<tr><th colspan="7">' . $OrganisationCategoryDescription . '</th></tr>';
+                                                echo '<tr class="zebra">';
                                                 echo '<th>Code</th>';
                                                 echo '<th>Name</th>';
                                                 echo '<th>Address</th>';
                                                 echo '<th>Contacts</th>';
                                                 echo '<th>Started Operating</th>';
+                                                echo '<th>Focal Person(s)</th>';
                                                 echo '<th>Umbrella Organisations</th>';
                                                 echo '</tr>';
 
+
+
                                                 $num_org = 0;
 
+                                                // Desplaying organisation details
                                                 foreach ($OrganisationCode as $data) {
-
-                                                    echo '<tr>';
+                                                    echo '<tr class="zebra">';
                                                     echo '<td>' . $data['OrganisationCode'] . '</td>';
-                                                    echo '<td>' . $data['OrganisationName'];
-
-                                                    if ($details === "all") {
-                                                        echo '<br> People:';
-                                                        foreach ($person[$data['OrganisationCode']] as $people) {
-                                                            echo '<br>' . $people['FullName'];
-                                                        }
-                                                    }
-                                                    echo '</td>';
-                                                    echo '<td>' . $data['PostalAddress'] . '</td>';
+                                                    echo '<td>' . $data['OrganisationName'] . '</td>';
+                                                    echo '<td>' . $data['PostalAddress'] . '<br>' . $data['PhysicalAddress'] . '</td>';
                                                     echo '<td> Tel: ' . $data['Phone'] . '<br> Fax: ' . $data['Fax'] . '</td>';
                                                     echo '<td>' . $data['StartDAte'] . '</td>';
+                                                    echo '<td>Zhapmos: ' . $data['ZHAPMosFocal'] . '<br>HIV: ' . $data['HIVPerson'] . '</td>';
                                                     echo '<td>' . $data['StartDAte'] . '</td>';
                                                     echo '</tr>';
+
+                                                    // Deplaying people details
+                                                    if ($details === "all") {
+
+                                                        echo '<tr>';
+                                                        echo '<td style="border-bottom: none; !important"></td>';
+                                                        echo '<td>Paeople:</td>';
+                                                        echo '<td>Desingnation</td>';
+                                                        echo '<td colspan="4">Contacts</td>';
+                                                        echo '</tr>';
+
+                                                        foreach ($person[$data['OrganisationCode']] as $people) {
+                                                            echo '<tr>';
+                                                            echo '<td></td>';
+                                                            echo '<td>' . $people['FullName'] . '</td>';
+                                                            echo '<td>' . $people['Designation'] . '</td>';
+                                                            echo '<td colspan="4">Tel: ' . $people['Phone'];
+                                                            echo '<br>Email: ' . $people['Email'];
+                                                            echo '</td>';
+                                                            echo '</tr>';
+                                                        }
+                                                    }
                                                     $total++;
                                                     $num_org++;
                                                 }
 
-                                                echo'<tr><td colspan="6" align="right">organisation total ' . $num_org . '</td><tr>';
+                                                echo'<tr><td colspan="7" align="right">organisation total ' . $num_org . '</td><tr>';
                                             }
                                             ?>
                                         </table>
