c769ebbfffccdfc3fda007628f59c85e3df93aa5
robby <robby@Robby-pc>
2012-11-01 12:52:50 +0300

    update

--- modules/data/form2.php
+++ modules/data/form2.php
@@ -85,7 +85,7 @@ require 'sections/lang_section.php';
                     <input type="hidden" name="lang" value="<?php echo $lang ?>">
                     <div class="data-form-wapper">
                         <?php $heading = $text["FORM_2_HEAD"]; ?>
-                       <?php require 'sections/head_section.php'; ?>
+                        <?php require 'sections/head_section.php'; ?>
                         <div class="section">
                             <h3><strong>A. <?php echo $text["SECT_HEAD_A2"]; ?></strong></h3>
                             <table width="100%" border="1" cellspacing="0" cellpadding="5" class="form-data-table">
@@ -108,20 +108,20 @@ require 'sections/lang_section.php';
                                                 <?php
                                             }
                                             ?>
-                                      </select>
-                                  </td>
+                                        </select>
+                                    </td>
                                     <td width="43" class="data-group">A1</td>
-                              </tr>
+                                </tr>
                                 <tr>
                                     <td><?php echo $text["SECT_LABEL_PHS"]; ?></td>
-                                  <td colspan="3"><input type="text" name="phy_addr" id="phy_addr" class="text" style="width: 98%"></td>
+                                    <td colspan="3"><input type="text" name="phy_addr" id="phy_addr" class="text" style="width: 98%"></td>
                                     <td class="data-group">A2</td>
-                              </tr>
+                                </tr>
                                 <tr>
                                     <td><?php echo $text["SECT_LABEL_SPO"]; ?></td>
-                                  <td colspan="3"><input type="text" name="post_addr" id="post_addr" class="text" style="width: 98%"></td>
+                                    <td colspan="3"><input type="text" name="post_addr" id="post_addr" class="text" style="width: 98%"></td>
                                     <td class="data-group">A3</td>
-                              </tr>
+                                </tr>
                                 <tr>
                                     <td><?php echo $text["SECT_LABEL_SDC"]; ?></td>
                                     <td colspan="3">
@@ -136,21 +136,21 @@ require 'sections/lang_section.php';
                                                 <?php
                                             }
                                             ?>
-                                      </select>
-                                  </td>
+                                        </select>
+                                    </td>
                                     <td class="data-group">A4</td>
-                              </tr>
+                                </tr>
                                 <tr>
                                     <td><?php echo $text["SECT_LABEL_FOP"]; ?></td>
-                                  <td colspan="3"><input type="text" name="focal_per" id="focal_per" class="text" style="width: 98%"></td>
+                                    <td colspan="3"><input type="text" name="focal_per" id="focal_per" class="text" style="width: 98%"></td>
                                     <td class="data-group">A5</td>
-                              </tr>
+                                </tr>
                                 <tr>
                                     <td rowspan="2"><?php echo $text["SECT_LABEL_CFP"]; ?></td>
-                                  <td colspan="2"><?php echo $text["SECT_LABEL_TEL"]; ?> <input type="tel" name="focal_tel" id="focal_tel" class="text" style="width: 78%"></td>
+                                    <td colspan="2"><?php echo $text["SECT_LABEL_TEL"]; ?> <input type="tel" name="focal_tel" id="focal_tel" class="text" style="width: 78%"></td>
                                     <td width="206"><?php echo $text["SECT_LABEL_FAX"]; ?> <input type="tel" name="focal_fax" id="focal_fax" class="text" style="width: 77%"></td>
                                     <td class="data-group">A6</td>
-                              </tr>
+                                </tr>
                                 <tr>
 
                                     <td colspan="3"><?php echo $text["SECT_LABEL_EML"]; ?> <input type="email" name="focal_email" id="focal_email" class="text" style="width: 80%"></td>
@@ -159,21 +159,21 @@ require 'sections/lang_section.php';
                                 <tr>
                                     <td><?php echo $text["SECT_LABEL_STY"]; ?></td>
                                     <td colspan="3">
-                                      <label><input type="radio" name="school_type" value="Primary" id="primary_school"> <?php echo $text["SECT_LABEL_PRM"] ?></label> &nbsp;
+                                        <label><input type="radio" name="school_type" value="Primary" id="primary_school"> <?php echo $text["SECT_LABEL_PRM"] ?></label> &nbsp;
                                         <label><input type="radio" name="school_type" value="Secondary" id="secondary_school"> <?php echo $text["SECT_LABEL_SCO"] ?></label>
-                                  </td>
+                                    </td>
                                     <td class="data-group">A8</td>
-                              </tr>
-                                
+                                </tr>
+
                                 <tr>
                                     <td width="272" rowspan="2"><?php echo $text["SECT_LABEL_SNO"]; ?></td>
                                     <td align="center" width="152"><?php echo $text["SECT_LABEL_MAL"]; ?></td>
-                                  <td align="center" width="160"><?php echo $text["SECT_LABEL_FEM"]; ?></td>
-                                  <td align="center"><?php echo $text["SECT_LABEL_TOT"]; ?></td>
+                                    <td align="center" width="160"><?php echo $text["SECT_LABEL_FEM"]; ?></td>
+                                    <td align="center"><?php echo $text["SECT_LABEL_TOT"]; ?></td>
                                     <td rowspan="2" class="data-group">A9</td>
-                              </tr>
+                                </tr>
                                 <tr>
-                                    
+
                                     <td><input type="number" name="std_male" id="std_male" min="0" class="number"></td>
                                     <td><input type="number" name="std_female" id="std_female" min="0" class="number"></td>
                                     <td>&nbsp;</td>
@@ -181,13 +181,43 @@ require 'sections/lang_section.php';
                                 <tr>
                                     <td><?php echo $text["SECT_LABEL_SOW"]; ?></td>
                                     <td colspan="3">
-                                      <label><input type="radio" name="school_owner" value="Government" id="primary_school"> <?php echo $text["SECT_LABEL_GOV"] ?></label> &nbsp;
+                                        <label><input type="radio" name="school_owner" value="Government" id="primary_school"> <?php echo $text["SECT_LABEL_GOV"] ?></label> &nbsp;
                                         <label><input type="radio" name="school_owner" value="Private" id="secondary_school"> <?php echo $text["SECT_LABEL_PRI"] ?></label>
-                                  </td>
+                                    </td>
                                     <td class="data-group">A10</td>
-                              </tr>
+                                </tr>
                             </table>
                             <!-- end .section  --></div>
+                        <div class="section">
+                            <h3><strong>D. <?php echo $text["SECT_HEAD_D"]; ?></strong></h3>
+                            <table width="100%" border="1" cellspacing="0">
+                                <tr>
+                                    <td></td>
+                                    <th width="104"><?php echo $BreakdownTypeDescription1["CS1"][1] ?></th>
+                                    <th width="104"><?php echo $BreakdownTypeDescription1["CS1"][0] ?></th>
+                                    <th width="104"><?php echo $BreakdownTypeDescription1["CS1"][2] ?></th>
+                                    <td rowspan="2"  width="60" class="data-group">CS1</td>
+                                </tr>
+                                <tr>
+                                    <td><?php echo $ZhaFigureDescription["CS1"][0]; ?></td>
+                                    <td><input type="number" name="cs1_males" min="0" class="number"></td>
+                                    <td><input type="number" name="cs1_females" min="0" class="number"></td>
+                                    <td></td>
+                                </tr>
+                            </table>
+
+                            <table width="100%" border="1" cellspacing="0">
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <th width="200"><?php echo $text["SECT_SUB_HEAD_CS_PVI"] ?></th>
+                                    <td rowspan="2" width="47" class="data-group">CS2</td>
+                                </tr>
+                                <tr>
+                                    <td><?php echo $ZhaFigureDescription["CS2"][0] ?></td>
+                                    <td><input type="number" name="cs2_person_visit" min="0" class="number"></td>
+                                </tr>
+                            </table>
+                        </div>
                         <?php require 'sections/footer_section.php'; ?>
                         <!-- end .data-form-wrapper  -->  </div>
                 </form>
