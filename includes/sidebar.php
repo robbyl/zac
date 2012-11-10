bfe725652e46386596edea24fdd3afb55f793d9c
robby <robby@Robby-pc>
2012-11-10 21:01:12 +0300

    update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -19,10 +19,9 @@
             <li><a href="../../modules/settings/settings.php" >General settings</a></li>
         </ul>
 
-        <h3 class="menuheader expandable backups">Backup</h3>
+        <h3 class="menuheader expandable backups">Backups</h3>
         <ul class="categoryitems">
             <li><a href="../../modules/backups/backups.php" >View backups</a></li>
-            <li><a href="../../modules/applications/view_application.php">View applications</a></li>
         </ul>
         <?php
     }
@@ -36,10 +35,9 @@
     ) {
         ?>
 
-        <h3 class="menuheader expandable customers">Customers</h3>
+        <h3 class="menuheader expandable data-entry">Data Entry</h3>
         <ul class="categoryitems">
-            <li><a href="../../modules/customers/customers.php" >View customers</a></li>
-            <li><a href="../../modules/customers/customer_status.php" >Customer status</a></li>
+            <li><a href="../../modules/data/data_entry.php" >Add various data</a></li>
         </ul>
 
         <?php
@@ -51,65 +49,11 @@
     ) {
         ?>
 
-        <h3 class="menuheader expandable meters">Water Meters</h3>
+        <h3 class="menuheader expandable printouts">Printouts</h3>
         <ul class="categoryitems">
-            <li><a href="../../modules/meters/add_meter.php" >Add meter</a></li>
-            <li><a href="../../modules/meters/meter_readings.php">View meter readings</a></li>
-            <li><a href="../../modules/meters/enter_meter_readings.php">Enter meter readings</a></li>
-            <li><a href="../../modules/meters/meter_sheet.php">Print reading sheets</a></li>
+            <li><a href="../../modules/printouts/printouts.php" >Print records</a></li>
         </ul>
 
-        <?php
-    }
-    if (
-    // Sales access
-            $_SESSION['role'] === "ROOT" ||
-            $_SESSION['role'] === "BILLING OFFICER" ||
-            $_SESSION['role'] === "ACCOUNTANT"
-    ) {
-        ?>
-
-        <h3 class="menuheader expandable invoices">Sales</h3>
-        <ul class="categoryitems">
-            <li><a href="../../modules/invoice/generate_invoices.php" >Generate invoices</a></li>
-            <li><a href="../../modules/invoice/invoices.php">View invoices</a></li>
-        </ul>
-        <?php
-    }
-    if (
-            $_SESSION['role'] === "ROOT" ||
-            $_SESSION['role'] == "CASHIER"
-    ) {
-        ?>
-
-        <h3 class="menuheader expandable financial">Pay Point</h3>
-        <ul class="categoryitems">
-            <li><a href="../../modules/paypoint/online_payments.php" >Online payments</a></li>
-            <li><a href="../../modules/paypoint/offline_payments.php">Offline payments</a></li>
-            <li><a href="../../modules/paypoint/transactions.php">Transactions</a></li>
-        </ul>
-
-        <?php
-    }
-    if (
-    // Adjustments access.
-            $_SESSION['role'] === "ROOT" ||
-            $_SESSION['role'] === "CREDIT CONTROLLER" ||
-            $_SESSION['role'] === "ACCOUNTANT" ||
-            $_SESSION['role'] === "BILLING OFFICER"
-    ) {
-        ?>
-
-        <h3 class="menuheader expandable adjustment">Adjustments</h3>
-        <ul class="categoryitems">
-            <li><a href="../../modules/adjustments/perform_adjustments.php">Perform adjustments</a></li>
-            <li><a href="../../modules/adjustments/view_adjustments.php" >View adjustments</a></li>
-        </ul>
     <?php } ?>
-
-    <h3 class="menuheader expandable reports" >Report Manager</h3>
-    <ul class="categoryitems">
-        <li><a href="../../modules/report/reports.php" >Generate reports</a></li>
-    </ul>
 </div>
 <?php session_commit(); ?>
--- modules/data/data_entry.php
+++ modules/data/data_entry.php
@@ -8,11 +8,12 @@ ob_start();
     <head>
         <meta charset="utf-8">
         <link rel="icon" href="../../favicon.ico" type="image/x-icon" />
-        <title>ZANHID | GENERATE INVOICES</title>
+        <title>ZANHID | DATA ENTRY</title>
         <link href="../../css/layout.css" rel="stylesheet" type="text/css">
         <script src="../../js/jquery-1.7.2.js" type="text/javascript"></script>
         <script src="../../js/zanhid-core.js" type="text/javascript"></script>
         <script src="../../js/accordion.js" type="text/javascript"></script>
+
         <style type="text/css">
             textarea {
                 font: inherit !important;
@@ -31,26 +32,6 @@ ob_start();
                 });
 
             });
-            ddaccordion.init({
-                headerclass: "expandable", //Shared CSS class name of headers group that are expandable
-                contentclass: "categoryitems", //Shared CSS class name of contents group
-                revealtype: "click", //Reveal content when user clicks or onmouseover the header? Valid value: "click", "clickgo", or "mouseover"
-                mouseoverdelay: 200, //if revealtype="mouseover", set delay in milliseconds before header expands onMouseover
-                collapseprev: true, //Collapse previous content (so only one open at any time)? true/false 
-                defaultexpanded: [1], //index of content(s) open by default [index1, index2, etc]. [] denotes no content
-                onemustopen: false, //Specify whether at least one header should be open always (so never all headers closed)
-                animatedefault: true, //Should contents open by default be animated into view?
-                persiststate: false, //persist state of opened contents within browser session?
-                toggleclass: ["", "openheader"], //Two CSS classes to be applied to the header when it's collapsed and expanded, respectively ["class1", "class2"]
-                togglehtml: ["prefix", "", ""], //Additional HTML added to the header when it's collapsed and expanded, respectively  ["position", "html1", "html2"] (see docs)
-                animatespeed: "fast", //speed of animation: integer in milliseconds (ie: 200), or keywords "fast", "normal", or "slow"
-                oninit: function(headers, expandedindices) { //custom code to run when headers have initalized
-                    //do nothing
-                },
-                onopenclose: function(header, index, state, isuseractivated) { //custom code to run whenever a header is opened or closed
-                    //do nothing
-                }
-            })
 
         </script>
     </head>
@@ -61,130 +42,7 @@ ob_start();
             <?php require '../../includes/header.php'; ?>
             <div id="pop-up"></div>
             <div class="sidebar">
-                <?php session_start(); ?>
-                <div class="arrowlistmenu">
-                    <a href="../../home.php"><h3 class="menuheader home">Home</h3></a>
-                    <?php
-                    if (
-                    // Manage users and settings access
-                            $_SESSION['role'] === "ROOT"
-                    ) {
-                        ?>
-                        <h3 class="menuheader expandable users">Manage Users</h3>
-                        <ul class="categoryitems">
-                            <li><a href="../../modules/users/new_user.php">Add new user</a></li>
-                            <li><a href="../../modules/users/users.php">View users</a></li>
-                        </ul>
-
-                        <h3 class="menuheader expandable settings">Settings</h3>
-                        <ul class="categoryitems">
-                            <li><a href="../../modules/settings/settings.php" >General settings</a></li>
-                            <li><a href="../../modules/settings/tariffs.php">Tariffs</a></li>
-                        </ul>
-                        <?php
-                    }
-
-                    if (
-                    // Application access.
-                            $_SESSION['role'] === "ROOT" ||
-                            $_SESSION['role'] === "CONNECTION OFFICER"
-                    ) {
-                        ?>
-
-                        <h3 class="menuheader expandable applications">Applications</h3>
-                        <ul class="categoryitems">
-                            <li><a href="../../modules/applications/add_new_appln.php" >Add application</a></li>
-                            <li><a href="../../modules/applications/view_application.php">View applications</a></li>
-                        </ul>
-                        <?php
-                    }
-
-                    if (
-                    // Customer access.
-                            $_SESSION['role'] === "ROOT" ||
-                            $_SESSION['role'] === "ACCOUNTANT" ||
-                            $_SESSION['role'] === "BILLING OFFICER" ||
-                            $_SESSION['role'] === "CREDIT CONTROLLER"
-                    ) {
-                        ?>
-
-                        <h3 class="menuheader expandable customers">Customers</h3>
-                        <ul class="categoryitems">
-                            <li><a href="../../modules/customers/customers.php" >View customers</a></li>
-                            <li><a href="../../modules/customers/customer_status.php" >Customer status</a></li>
-                        </ul>
-
-                        <?php
-                    }
-                    if (
-                    // Water meter access.
-                            $_SESSION['role'] === "ROOT" ||
-                            $_SESSION['role'] === "BILLING OFFICER"
-                    ) {
-                        ?>
-
-                        <h3 class="menuheader expandable meters">Water Meters</h3>
-                        <ul class="categoryitems">
-                            <li><a href="../../modules/meters/add_meter.php" >Add meter</a></li>
-                            <li><a href="../../modules/meters/meter_readings.php">View meter readings</a></li>
-                            <li><a href="../../modules/meters/enter_meter_readings.php">Enter meter readings</a></li>
-                            <li><a href="../../modules/meters/meter_sheet.php">Print reading sheets</a></li>
-                        </ul>
-
-                        <?php
-                    }
-                    if (
-                    // Sales access
-                            $_SESSION['role'] === "ROOT" ||
-                            $_SESSION['role'] === "BILLING OFFICER" ||
-                            $_SESSION['role'] === "ACCOUNTANT"
-                    ) {
-                        ?>
-
-                        <h3 class="menuheader expandable invoices">Sales</h3>
-                        <ul class="categoryitems">
-                            <li><a href="../../modules/invoice/generate_invoices.php" >Generate invoices</a></li>
-                            <li><a href="../../modules/invoice/invoices.php">View invoices</a></li>
-                        </ul>
-                        <?php
-                    }
-                    if (
-                            $_SESSION['role'] === "ROOT" ||
-                            $_SESSION['role'] == "CASHIER"
-                    ) {
-                        ?>
-
-                        <h3 class="menuheader expandable financial">Pay Point</h3>
-                        <ul class="categoryitems">
-                            <li><a href="../../modules/paypoint/online_payments.php" >Online payments</a></li>
-                            <li><a href="../../modules/paypoint/offline_payments.php">Offline payments</a></li>
-                            <li><a href="../../modules/paypoint/transactions.php">Transactions</a></li>
-                        </ul>
-
-                        <?php
-                    }
-                    if (
-                    // Adjustments access.
-                            $_SESSION['role'] === "ROOT" ||
-                            $_SESSION['role'] === "CREDIT CONTROLLER" ||
-                            $_SESSION['role'] === "ACCOUNTANT" ||
-                            $_SESSION['role'] === "BILLING OFFICER"
-                    ) {
-                        ?>
-
-                        <h3 class="menuheader expandable adjustment">Adjustments</h3>
-                        <ul class="categoryitems">
-                            <li><a href="../../modules/adjustments/perform_adjustments.php">Perform adjustments</a></li>
-                            <li><a href="../../modules/adjustments/view_adjustments.php" >View adjustments</a></li>
-                        </ul>
-                    <?php } ?>
-
-                    <h3 class="menuheader expandable reports">Report Manager</h3>
-                    <ul class="categoryitems">
-                        <li><a href="../../modules/report/reports.php" >Generate reports</a></li>
-                    </ul>
-                </div>
-                <?php session_commit(); ?>
+                <?php include '../../includes/sidebar.php'; ?>
                 <!-- end .sidebar --></div>
             <div class="content">
                 <?php
@@ -217,4 +75,30 @@ ob_start();
             <?php include '../../includes/footer.php'; ?>
             <!-- end .container --></div>
     </body>
+    <script type="text/javascript">
+
+        $('.data-entry').attr("id", "current");
+        var i = $('h3#current').index('.menuheader') - 1;
+
+        ddaccordion.init({
+            headerclass: "expandable", //Shared CSS class name of headers group that are expandable
+            contentclass: "categoryitems", //Shared CSS class name of contents group
+            revealtype: "click", //Reveal content when user clicks or onmouseover the header? Valid value: "click", "clickgo", or "mouseover"
+            mouseoverdelay: 200, //if revealtype="mouseover", set delay in milliseconds before header expands onMouseover
+            collapseprev: true, //Collapse previous content (so only one open at any time)? true/false
+            defaultexpanded: [i], //index of content(s) open by default [index1, index2, etc]. [] denotes no content
+            onemustopen: false, //Specify whether at least one header should be open always (so never all headers closed)
+            animatedefault: true, //Should contents open by default be animated into view?
+            persiststate: false, //persist state of opened contents within browser session?
+            toggleclass: ["", "openheader"], //Two CSS classes to be applied to the header when it's collapsed and expanded, respectively ["class1", "class2"]
+            togglehtml: ["prefix", "", ""], //Additional HTML added to the header when it's collapsed and expanded, respectively  ["position", "html1", "html2"] (see docs)
+            animatespeed: "fast", //speed of animation: integer in milliseconds (ie: 200), or keywords "fast", "normal", or "slow"
+            oninit: function(headers, expandedindices) { //custom code to run when headers have initalized
+                //do nothing
+            },
+            onopenclose: function(header, index, state, isuseractivated) { //custom code to run whenever a header is opened or closed
+                //do nothing
+            }
+        });
+    </script>
 </html>
